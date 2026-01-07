5099c9255e44782c8399ae25e75c109bbac77f3c
paparazzi-rn <tipaparazzirn@gmail.com>
2026-01-07 18:38:43 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,189 @@
+function ingles() {
+    const bemvindo = document.getElementById('bemvindo')    // NÃO MEXER
+    const cliqueabaixo = document.getElementById('cliqueabaixo')    // NÃO MEXER
+
+    const desc1 = document.getElementById('desc1')    // NÃO MEXER
+    const desc2 = document.getElementById('desc2')    // NÃO MEXER
+    const desc3 = document.getElementById('desc3')    // NÃO MEXER
+
+    const desc4 = document.getElementById('desc4')    // NÃO MEXER
+    const desc5 = document.getElementById('desc5')    // NÃO MEXER
+    const desc6 = document.getElementById('desc6')    // NÃO MEXER
+
+    const desc7 = document.getElementById('desc7')    // NÃO MEXER
+    const desc8 = document.getElementById('desc8')    // NÃO MEXER
+    const desc9 = document.getElementById('desc9')    // NÃO MEXER
+
+    const desc10 = document.getElementById('desc10')    // NÃO MEXER
+    const desc11 = document.getElementById('desc11')    // NÃO MEXER
+    const desc12 = document.getElementById('desc12')    // NÃO MEXER
+
+    const desc13 = document.getElementById('desc13')    // NÃO MEXER
+    const desc14 = document.getElementById('desc14')    // NÃO MEXER
+    const desc15 = document.getElementById('desc15')    // NÃO MEXER
+
+    const popuppresente = document.getElementById('popuppresente');    // NÃO MEXER
+    popuppresente.style.display = "flex";    // NÃO MEXER
+
+    const resgatar = document.getElementById('resgatar')    // NÃO MEXER
+    const ganhoupresente = document.getElementById('ganhoupresente');    // NÃO MEXER
+
+    //LISTA ABAIXO
+
+    bemvindo.innerText = 'welcome!';    //ALTERAR ENTRE AS ASPAS
+    cliqueabaixo.innerText = 'click on the icons below';    //ALTERAR ENTRE AS ASPAS
+
+    desc1.innerText = 'Special Offers';    //ALTERAR ENTRE AS ASPAS
+    desc2.innerText = 'Paparazzi Photography';    //ALTERAR ENTRE AS ASPAS
+    desc3.innerText = 'Gastronomy';    //ALTERAR ENTRE AS ASPAS
+
+    desc4.innerText = 'Leisure area';    //ALTERAR ENTRE AS ASPAS
+    desc5.innerText = 'Information';    //ALTERAR ENTRE AS ASPAS
+    desc6.innerText = 'Git Shop';    //ALTERAR ENTRE AS ASPAS
+
+    desc7.innerText = 'Tours';    //ALTERAR ENTRE AS ASPAS
+    desc8.innerText = 'Cocierge';    //ALTERAR ENTRE AS ASPAS
+    desc9.innerText = 'Spotify';    //ALTERAR ENTRE AS ASPAS
+
+    desc10.innerText = 'Minibar';    //ALTERAR ENTRE AS ASPAS
+    desc11.innerText = 'Free Shuttle';    //ALTERAR ENTRE AS ASPAS
+    desc12.innerText = 'WhatsApp';    //ALTERAR ENTRE AS ASPAS
+
+    desc13.innerText = 'Restaurant';    //ALTERAR ENTRE AS ASPAS
+    desc14.innerText = 'Schedule';    //ALTERAR ENTRE AS ASPAS
+    desc15.innerText = 'How To Get Here';    //ALTERAR ENTRE AS ASPAS
+
+    resgatar.innerText = 'rescue';    //ALTERAR ENTRE AS ASPAS
+    ganhoupresente.innerText = 'you got a gift!';    //ALTERAR ENTRE AS ASPAS
+}
+
+function espanhol() {
+    const bemvindo = document.getElementById('bemvindo')    // NÃO MEXER
+    const cliqueabaixo = document.getElementById('cliqueabaixo')    // NÃO MEXER
+
+    const popuppresente = document.getElementById('popuppresente');    // NÃO MEXER
+    popuppresente.style.display = "flex";    // NÃO MEXER
+
+    const desc1 = document.getElementById('desc1')    // NÃO MEXER
+    const desc2 = document.getElementById('desc2')    // NÃO MEXER
+    const desc3 = document.getElementById('desc3')    // NÃO MEXER
+
+    const desc4 = document.getElementById('desc4')    // NÃO MEXER
+    const desc5 = document.getElementById('desc5')    // NÃO MEXER
+    const desc6 = document.getElementById('desc6')    // NÃO MEXER
+
+    const desc7 = document.getElementById('desc7')    // NÃO MEXER
+    const desc8 = document.getElementById('desc8')    // NÃO MEXER
+    const desc9 = document.getElementById('desc9')    // NÃO MEXER
+
+    const desc10 = document.getElementById('desc10')    // NÃO MEXER
+    const desc11 = document.getElementById('desc11')    // NÃO MEXER
+    const desc12 = document.getElementById('desc12')    // NÃO MEXER
+
+    const desc13 = document.getElementById('desc13')    // NÃO MEXER
+    const desc14 = document.getElementById('desc14')    // NÃO MEXER
+    const desc15 = document.getElementById('desc15')    // NÃO MEXER
+    
+    const resgatar = document.getElementById('resgatar')    // NÃO MEXER
+    const ganhoupresente = document.getElementById('ganhoupresente');    // NÃO MEXER
+
+    // LISTA ABAIXO
+
+    bemvindo.innerText = '¡bienvenido!';    //ALTERAR ENTRE AS ASPAS
+    cliqueabaixo.innerText = 'haga clic en los íconos abajo';   //ALTERAR ENTRE AS ASPAS
+
+    desc1.innerText = 'Promociones';    //ALTERAR ENTRE AS ASPAS
+    desc2.innerText = 'Paparazzi Fotografia';    //ALTERAR ENTRE AS ASPAS
+    desc3.innerText = 'Gastronomía';    //ALTERAR ENTRE AS ASPAS
+
+    desc4.innerText = 'Área de ocio';    //ALTERAR ENTRE AS ASPAS
+    desc5.innerText = 'Información';    //ALTERAR ENTRE AS ASPAS
+    desc6.innerText = 'Tienda de regalos';    //ALTERAR ENTRE AS ASPAS
+
+    desc7.innerText = 'Excursiones';    //ALTERAR ENTRE AS ASPAS
+    desc8.innerText = 'Conserjería';    //ALTERAR ENTRE AS ASPAS
+    desc9.innerText = 'Spotify';    //ALTERAR ENTRE AS ASPAS
+
+    desc10.innerText = 'Minibar';    //ALTERAR ENTRE AS ASPAS
+    desc11.innerText = 'Transporte Gratuito';    //ALTERAR ENTRE AS ASPAS
+    desc12.innerText = 'WhatsApp';    //ALTERAR ENTRE AS ASPAS
+
+    desc13.innerText = 'Restaurante';    //ALTERAR ENTRE AS ASPAS
+    desc14.innerText = 'Programación';    //ALTERAR ENTRE AS ASPAS
+    desc15.innerText = 'Cómo llegar allí';    //ALTERAR ENTRE AS ASPAS
+
+
+    resgatar.innerText = 'rescate';    //ALTERAR ENTRE AS ASPAS
+    ganhoupresente.innerText = 'ganó un regalo!';    //ALTERAR ENTRE AS ASPAS
+}
+
+function portugues() {
+    const bemvindo = document.getElementById('bemvindo')    // NÃO MEXER
+    const cliqueabaixo = document.getElementById('cliqueabaixo')    // NÃO MEXER
+
+    const popuppresente = document.getElementById('popuppresente');    // NÃO MEXER
+    popuppresente.style.display = "flex";    // NÃO MEXER
+
+    const desc1 = document.getElementById('desc1')    // NÃO MEXER
+    const desc2 = document.getElementById('desc2')    // NÃO MEXER
+    const desc3 = document.getElementById('desc3')    // NÃO MEXER
+
+    const desc4 = document.getElementById('desc4')    // NÃO MEXER
+    const desc5 = document.getElementById('desc5')    // NÃO MEXER
+    const desc6 = document.getElementById('desc6')    // NÃO MEXER
+
+    const desc7 = document.getElementById('desc7')    // NÃO MEXER
+    const desc8 = document.getElementById('desc8')    // NÃO MEXER
+    const desc9 = document.getElementById('desc9')    // NÃO MEXER
+
+    const desc10 = document.getElementById('desc10')    // NÃO MEXER
+    const desc11 = document.getElementById('desc11')    // NÃO MEXER
+    const desc12 = document.getElementById('desc12')    // NÃO MEXER
+
+    const desc13 = document.getElementById('desc13')    // NÃO MEXER
+    const desc14 = document.getElementById('desc14')    // NÃO MEXER
+    const desc15 = document.getElementById('desc15')    // NÃO MEXER
+
+    const resgatar = document.getElementById('resgatar')    // NÃO MEXER
+    const ganhoupresente = document.getElementById('ganhoupresente');    // NÃO MEXER
+
+    // LISTA ABAIXO
+
+    bemvindo.innerText = 'seja bem vindo!';    //ALTERAR ENTRE AS ASPAS
+    cliqueabaixo.innerText = 'clique nos ícones abaixo';    //ALTERAR ENTRE AS ASPAS
+
+    desc1.innerText = 'Promoções';    //ALTERAR ENTRE AS ASPAS
+    desc2.innerText = 'Paparazzi Fotografia';    //ALTERAR ENTRE AS ASPAS
+    desc3.innerText = 'Gastronomia';    //ALTERAR ENTRE AS ASPAS
+
+    desc4.innerText = 'Área de lazer';    //ALTERAR ENTRE AS ASPAS
+    desc5.innerText = 'Informações';    //ALTERAR ENTRE AS ASPAS
+    desc6.innerText = 'Lojinha';    //ALTERAR ENTRE AS ASPAS
+
+    desc7.innerText = 'Passeio';    //ALTERAR ENTRE AS ASPAS
+    desc8.innerText = 'Cocierge';    //ALTERAR ENTRE AS ASPAS
+    desc9.innerText = 'Spotify';    //ALTERAR ENTRE AS ASPAS
+
+    desc10.innerText = 'Frigobar';    //ALTERAR ENTRE AS ASPAS
+    desc11.innerText = 'Freebus';    //ALTERAR ENTRE AS ASPAS
+    desc12.innerText = 'WhatsApp';    //ALTERAR ENTRE AS ASPAS
+
+    desc13.innerText = 'Restaurante';    //ALTERAR ENTRE AS ASPAS
+    desc14.innerText = 'Programação';    //ALTERAR ENTRE AS ASPAS
+    desc15.innerText = 'Como Chegar!';    //ALTERAR ENTRE AS ASPAS
+
+    resgatar.innerText = 'resgatar';    //ALTERAR ENTRE AS ASPAS
+    ganhoupresente.innerText = 'você ganhou um presente!';    //ALTERAR ENTRE AS ASPAS
+}
+
+// NÃO MEXER ABAIXO
+
+function fechar() {
+    const popuppresente = document.getElementById('popuppresente');
+    popuppresente.style.display = "none";
+}
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    return portugues()
+});
