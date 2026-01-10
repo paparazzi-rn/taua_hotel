c7b3dfd31ea7ac95a8bf5d855925351256ba89fd
paparazzi-rn <tipaparazzirn@gmail.com>
2026-01-10 12:16:36 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -23,7 +23,9 @@ function ingles() {
     const desc15 = document.getElementById('desc15')    // NÃO MEXER
 
     const popuppresente = document.getElementById('popuppresente');    // NÃO MEXER
-    popuppresente.style.display = "flex";    // NÃO MEXER
+    if (popuppresente) {
+        popuppresente.style.display = "flex";    // NÃO MEXER
+    }
 
     const resgatar = document.getElementById('resgatar')    // NÃO MEXER
     const ganhoupresente = document.getElementById('ganhoupresente');    // NÃO MEXER
@@ -58,11 +60,16 @@ function ingles() {
 }
 
 function espanhol() {
+
+    // TELA INICIAL
+
     const bemvindo = document.getElementById('bemvindo')    // NÃO MEXER
     const cliqueabaixo = document.getElementById('cliqueabaixo')    // NÃO MEXER
 
     const popuppresente = document.getElementById('popuppresente');    // NÃO MEXER
-    popuppresente.style.display = "flex";    // NÃO MEXER
+    if (popuppresente) {
+        popuppresente.style.display = "flex";    // NÃO MEXER
+    }
 
     const desc1 = document.getElementById('desc1')    // NÃO MEXER
     const desc2 = document.getElementById('desc2')    // NÃO MEXER
@@ -89,32 +96,35 @@ function espanhol() {
 
     // LISTA ABAIXO
 
-    bemvindo.innerText = '¡bienvenido!';    //ALTERAR ENTRE AS ASPAS
-    cliqueabaixo.innerText = 'haga clic en los íconos abajo';   //ALTERAR ENTRE AS ASPAS
+    if (bemvindo, cliqueabaixo, desc1, desc2, desc3, desc4, desc5, desc6, desc7, desc8, desc9, desc10, desc11, desc12, desc13, desc14, desc15, resgatar, ganhoupresente) {
+        bemvindo.innerText = '¡bienvenido!';    //ALTERAR ENTRE AS ASPAS
+        cliqueabaixo.innerText = 'haga clic en los íconos abajo';   //ALTERAR ENTRE AS ASPAS
+    
+        desc1.innerText = 'Promociones';    //ALTERAR ENTRE AS ASPAS
+        desc2.innerText = 'Paparazzi Fotografia';    //ALTERAR ENTRE AS ASPAS
+        desc3.innerText = 'Gastronomía';    //ALTERAR ENTRE AS ASPAS
+    
+        desc4.innerText = 'Área de ocio';    //ALTERAR ENTRE AS ASPAS
+        desc5.innerText = 'Información';    //ALTERAR ENTRE AS ASPAS
+        desc6.innerText = 'Tienda de regalos';    //ALTERAR ENTRE AS ASPAS
+    
+        desc7.innerText = 'Excursiones';    //ALTERAR ENTRE AS ASPAS
+        desc8.innerText = 'Conserjería';    //ALTERAR ENTRE AS ASPAS
+        desc9.innerText = 'Spotify';    //ALTERAR ENTRE AS ASPAS
+    
+        desc10.innerText = 'Minibar';    //ALTERAR ENTRE AS ASPAS
+        desc11.innerText = 'Limpieza';    //ALTERAR ENTRE AS ASPAS
+        desc12.innerText = 'WhatsApp';    //ALTERAR ENTRE AS ASPAS
+    
+        desc13.innerText = 'Restaurante';    //ALTERAR ENTRE AS ASPAS
+        desc14.innerText = 'Programación';    //ALTERAR ENTRE AS ASPAS
+        desc15.innerText = 'Cómo llegar allí';    //ALTERAR ENTRE AS ASPAS
+    
+    
+        resgatar.innerText = 'rescate';    //ALTERAR ENTRE AS ASPAS
+        ganhoupresente.innerText = 'ganó un regalo!';    //ALTERAR ENTRE AS ASPAS
+    }
 
-    desc1.innerText = 'Promociones';    //ALTERAR ENTRE AS ASPAS
-    desc2.innerText = 'Paparazzi Fotografia';    //ALTERAR ENTRE AS ASPAS
-    desc3.innerText = 'Gastronomía';    //ALTERAR ENTRE AS ASPAS
-
-    desc4.innerText = 'Área de ocio';    //ALTERAR ENTRE AS ASPAS
-    desc5.innerText = 'Información';    //ALTERAR ENTRE AS ASPAS
-    desc6.innerText = 'Tienda de regalos';    //ALTERAR ENTRE AS ASPAS
-
-    desc7.innerText = 'Excursiones';    //ALTERAR ENTRE AS ASPAS
-    desc8.innerText = 'Conserjería';    //ALTERAR ENTRE AS ASPAS
-    desc9.innerText = 'Spotify';    //ALTERAR ENTRE AS ASPAS
-
-    desc10.innerText = 'Minibar';    //ALTERAR ENTRE AS ASPAS
-    desc11.innerText = 'Limpieza';    //ALTERAR ENTRE AS ASPAS
-    desc12.innerText = 'WhatsApp';    //ALTERAR ENTRE AS ASPAS
-
-    desc13.innerText = 'Restaurante';    //ALTERAR ENTRE AS ASPAS
-    desc14.innerText = 'Programación';    //ALTERAR ENTRE AS ASPAS
-    desc15.innerText = 'Cómo llegar allí';    //ALTERAR ENTRE AS ASPAS
-
-
-    resgatar.innerText = 'rescate';    //ALTERAR ENTRE AS ASPAS
-    ganhoupresente.innerText = 'ganó un regalo!';    //ALTERAR ENTRE AS ASPAS
 }
 
 function portugues() {
@@ -156,27 +166,27 @@ function portugues() {
     if (bemvindo, cliqueabaixo, desc1, desc2, desc3, desc4, desc5, desc6, desc7, desc8, desc9, desc10, desc11, desc12, desc13, desc14, desc15, resgatar, ganhoupresente) {
         bemvindo.innerText = 'seja bem vindo!';    //ALTERAR ENTRE AS ASPAS
         cliqueabaixo.innerText = 'clique nos ícones abaixo';    //ALTERAR ENTRE AS ASPAS
-    
+
         desc1.innerText = 'Promoções';    //ALTERAR ENTRE AS ASPAS
         desc2.innerText = 'Paparazzi Fotografia';    //ALTERAR ENTRE AS ASPAS
         desc3.innerText = 'Gastronomia';    //ALTERAR ENTRE AS ASPAS
-    
+
         desc4.innerText = 'Área de lazer';    //ALTERAR ENTRE AS ASPAS
         desc5.innerText = 'Informações';    //ALTERAR ENTRE AS ASPAS
         desc6.innerText = 'Lojinha';    //ALTERAR ENTRE AS ASPAS
-    
+
         desc7.innerText = 'Passeio';    //ALTERAR ENTRE AS ASPAS
         desc8.innerText = 'Cocierge';    //ALTERAR ENTRE AS ASPAS
         desc9.innerText = 'Spotify';    //ALTERAR ENTRE AS ASPAS
-    
+
         desc10.innerText = 'Frigobar';    //ALTERAR ENTRE AS ASPAS
         desc11.innerText = 'Limpeza';    //ALTERAR ENTRE AS ASPAS
         desc12.innerText = 'WhatsApp';    //ALTERAR ENTRE AS ASPAS
-    
+
         desc13.innerText = 'Restaurante';    //ALTERAR ENTRE AS ASPAS
         desc14.innerText = 'Programação';    //ALTERAR ENTRE AS ASPAS
         desc15.innerText = 'Como Chegar!';    //ALTERAR ENTRE AS ASPAS
-    
+
         resgatar.innerText = 'resgatar';    //ALTERAR ENTRE AS ASPAS
         ganhoupresente.innerText = 'você ganhou um presente!';    //ALTERAR ENTRE AS ASPAS
     }
@@ -189,11 +199,14 @@ function portugues() {
     const naopertube = document.getElementById('naopertube')    // NÃO MEXER
     const precisotoalha = document.getElementById('precisotoalha')    // NÃO MEXER
 
-    limpezaquarto.innerText = 'limpeza de quarto';    //ALTERAR ENTRE AS ASPAS
-    parasolicitar.innerText = 'para solicitar clique nos ícones abaixo:';    //ALTERAR ENTRE AS ASPAS
-    limparquarto.innerText = 'Limpar Quarto';    //ALTERAR ENTRE AS ASPAS
-    naopertube.innerText = 'Não Pertube';    //ALTERAR ENTRE AS ASPAS
-    precisotoalha.innerText = 'Preciso de Toalhas';    //ALTERAR ENTRE AS ASPAS
+    if (limpezaquarto, parasolicitar, limparquarto, naopertube, precisotoalha) {
+        limpezaquarto.innerText = 'limpeza de quarto';    //ALTERAR ENTRE AS ASPAS
+        parasolicitar.innerText = 'para solicitar clique nos ícones abaixo:';    //ALTERAR ENTRE AS ASPAS
+        limparquarto.innerText = 'Limpar Quarto';    //ALTERAR ENTRE AS ASPAS
+        naopertube.innerText = 'Não Pertube';    //ALTERAR ENTRE AS ASPAS
+        precisotoalha.innerText = 'Preciso de Toalhas';    //ALTERAR ENTRE AS ASPAS
+    }
+
 }
 
 // NÃO MEXER ABAIXO
